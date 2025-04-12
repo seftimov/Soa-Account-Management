dc0a4d285274e1c9e5c1c08854813c296a90a7ca
DarkoSasanski <darko.sasanski@gmail.com>
2025-04-12 18:39:14 +0200

    Exercise 5: Kafka event messaging refactor - topic conventions

--- src/main/java/mk/ukim/finki/soa/accountmanagement/infrastructure/KafkaEventConsumer.kt
+++ src/main/java/mk/ukim/finki/soa/accountmanagement/infrastructure/KafkaEventConsumer.kt
@@ -21,7 +21,7 @@ class KafkaEventConsumer(
         .registerModules(KotlinModule.Builder().build())
         .registerModule(JavaTimeModule())
 
-    @KafkaListener(topics = ["finance-event"])
+    @KafkaListener(topics = ["finance_management.expense.created"])
     fun listen(record: ConsumerRecord<String, String>) {
 
         try {
